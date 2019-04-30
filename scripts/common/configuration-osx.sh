865c0c587154515397eba5db06ece62fcf0f921c
Giorgio Dal Toe <gdaltoe@mobiquityinc.com>
2019-04-30 13:51:18 +0200

    added keyboard cursor speed

--- scripts/common/configuration-osx.sh
+++ scripts/common/configuration-osx.sh
@@ -1,6 +1,10 @@
 echo
 echo 'Customizing OS X configuration'
 
+#  fast key repeat rate, requires reboot to take effect
+defaults write /Users/pivotal/Library/Preferences/.GlobalPreferences KeyRepeat -int 1
+defaults write /Users/pivotal/Library/Preferences/.GlobalPreferences InitialKeyRepeat -int 15
+
 # set finder to display full path in title bar
 defaults write com.apple.finder '_FXShowPosixPathInTitle' -bool true
 
