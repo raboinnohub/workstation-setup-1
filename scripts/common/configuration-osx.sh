996ce6060ed501a18fa14685129afbc929ca5f1d
Giorgio Dal Toe <gdaltoe@mobiquityinc.com>
2019-05-01 13:26:52 +0200

    fixed osx user preferences

--- scripts/common/configuration-osx.sh
+++ scripts/common/configuration-osx.sh
@@ -1,13 +1,13 @@
 echo
 echo 'Customizing OS X configuration'
 
-#  fast key repeat rate, requires reboot to take effect
-defaults write /Users/pivotal/Library/Preferences/.GlobalPreferences KeyRepeat -int 1
-defaults write /Users/pivotal/Library/Preferences/.GlobalPreferences InitialKeyRepeat -int 15
+echo 'Change to fast key repeat rate, requires reboot to take effect'
+defaults write /Users/$USER/Library/Preferences/.GlobalPreferences KeyRepeat -int 1
+defaults write /Users/$USER/Library/Preferences/.GlobalPreferences InitialKeyRepeat -int 15
 
-# set finder to display full path in title bar
+echo 'set finder to display full path in title bar'
 defaults write com.apple.finder '_FXShowPosixPathInTitle' -bool true
 
-# stop Photos from opening automatically
+echo 'stop Photos from opening automatically'
 defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
 #to revert use defaults -currentHost delete com.apple.ImageCapture disableHotPlug
